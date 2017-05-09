7f2d45608d67848a10370a451ef483431b73f504
sungjp <gpark18@amherst.edu>
2017-05-08 21:08:00 -0400

    added ambidext mousekeys to unused keys

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  {KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_ENT,  KC_SPC,  M(2),    KC_RGUI, _______,    M(5),    BL_TOGG}
+  {M(6),    KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_ENT,  KC_SPC,  M(2),    KC_RGUI, _______, M(5),    M(6)}
 }, 
 
 // Lower
@@ -61,6 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_ACL1, KC_ACL2, _______, _______}
+},
+
+/* Ambi Mousekeys */
+[5] = {
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_WH_L, KC_WH_U, KC_MS_U, KC_WH_D, KC_WH_R, _______, _______, KC_WH_L, KC_WH_U, KC_MS_U, KC_WH_D, KC_WH_R},
+  {_______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______},
+  {_______, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, KC_BTN3, KC_BTN1, KC_BTN2, _______, _______}
 }
 
 };
@@ -101,6 +109,13 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           layer_on(3);
       } else {
           layer_off(3);
+      }
+      break;
+    case 6:
+      if (record->event.pressed) {
+          layer_on(5);
+      } else {
+          layer_off(5);
       }
       break;
   }

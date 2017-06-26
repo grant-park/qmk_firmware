dccab6cb4613d778d724a938acecda988cfcfc1c
sungjp <gpark18@amherst.edu>
2017-06-26 06:36:43 -0400

    map

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -28,22 +28,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  {KC_LCTL,    KC_LALT, KC_LGUI, M(1), KC_ENT,  _______, KC_SPC,  M(2),    M(5),    _______, KC_RGUI,  _______}
+  {KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_ENT,  _______, KC_SPC,  M(2),    M(5),    _______, KC_RGUI,  _______}
 }, 
 
 // Lower
+
+// CLEAN, SYNC, SEARCH_PROJ, SEARCH_CLASS
 [1] = {
   {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS},
-  {KC_TILD, KC_F1,   KC_F2,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, KC_LCBR, KC_RCBR, KC_PIPE},
-  {MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______  , BL_DEC,  BL_INC,  _______, _______, _______, _______, _______, KC_J,    KC_9,    KC_K,    _______}
+  {KC_TILD, M(3),    M(4),    M(6),    M(7),    _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_PIPE},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 // Raise
 [2] = {
   {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL },
-  {KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_LBRC, KC_RBRC, KC_BSLS},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
@@ -61,14 +63,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_ACL0, KC_ACL1, KC_BTN1, KC_BTN2, KC_BTN3, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_ACL1, KC_ACL2, _______, _______}
-},
-
-/* Ambi Mousekeys */
-[5] = {
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_WH_L, KC_WH_U, KC_MS_U, KC_WH_D, KC_WH_R, _______, _______, KC_WH_L, KC_WH_U, KC_MS_U, KC_WH_D, KC_WH_R},
-  {_______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______},
-  {_______, KC_BTN3, KC_BTN1, KC_BTN2, KC_ACL0, _______, _______, KC_ACL0, KC_BTN1, KC_BTN2, KC_BTN3, _______}
 }
 
 };
@@ -95,13 +89,15 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       }
       break;
     case 3:
+      // clean
       if (record->event.pressed) {
-          return MACRO(D(LCTL),T(LEFT),U(LCTL),END);
+          return MACRO(D(LGUI),D(LSFT),T(C),U(LGUI),U(LSFT),END);
       }
       break;
     case 4:
+      // sync
       if (record->event.pressed) {
-          return MACRO(D(LCTL),T(RGHT),U(LCTL),END);
+          return MACRO(D(LGUI),D(LALT),T(S),U(LALT),U(LGUI),END);
       }
       break;
     case 5:
@@ -112,19 +108,17 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       }
       break;
     case 6:
+      // search proj
       if (record->event.pressed) {
-          layer_on(5);
-      } else {
-          layer_off(5);
+          return MACRO(D(LGUI),D(LSFT),T(F),U(LGUI),U(LSFT),END);
       }
       break;
-    // layer 7
-    case 7: 
+    case 7:
+      // search class
       if (record->event.pressed) {
-          layer_on(7);
-      } else {
-          layer_off(7);
+          return MACRO(D(LGUI),D(LSFT),T(O),U(LGUI),U(LSFT),END);
       }
+      break;
   }
   return MACRO_NONE;
 

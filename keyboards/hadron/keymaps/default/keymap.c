ff58bedf1d4b580447a0cb24e4336a267c4cdf3c
gpark <gpark@wayfair.com>
2017-08-19 15:20:23 -0400

    update

--- keyboards/hadron/keymaps/default/keymap.c
+++ keyboards/hadron/keymaps/default/keymap.c
@@ -60,6 +60,7 @@ enum preonic_keycodes {
   SEARCH_PROJ,
   SEARCH_CLASS,
   REFORMAT,
+  OPTIMPORTS,
   HORIZ,
   VERT,
   TMUX,
@@ -72,6 +73,7 @@ enum macro_keycodes {
   KC_SEARCH_PROJ,
   KC_SEARCH_CLASS,
   KC_REFORMAT,
+  KC_OPTIMPORTS,
   KC_HORIZ,
   KC_VERT,
   KC_TMUX,
@@ -94,6 +96,7 @@ enum macro_keycodes {
 #define SEARCH_PROJ M(KC_SEARCH_PROJ)
 #define SEARCH_CLASS M(KC_SEARCH_CLASS)
 #define REFORMAT    M(KC_REFORMAT)
+#define OPTIMPORTS  M(KC_OPTIMPORTS)
 #define HORIZ       M(KC_HORIZ) 
 #define VERT        M(KC_VERT)
 #define TMUX        M(KC_TMUX)
@@ -116,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP( 
   KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F10,  KC_F11,  KC_F12,   KC_F7,   KC_F8,   KC_F9,\
   KC_ESC,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,    KC_Y,    KC_U,    KC_I,     KC_O,       KC_P,            KC_BSPC, \
-  KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,    KC_H,    KC_J,    KC_K,     KC_L,       LT_MC(KC_SCLN),  KC_QUOT, \
+  KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,    KC_H,    KC_J,    KC_K,     KC_L,       KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,    KC_N,    KC_M,    KC_COMM,  KC_DOT,     KC_SLSH,         KC_RSFT, \
   KC_GRV,   KC_LCTRL, KC_LALT, KC_LGUI, LOWER,   KC_ENT,  KC_P0,   KC_DOT,  KC_EQL,   KC_SPC,  RAISE,   KC_RGUI,  KC_RALT,    ARROWS,          KC_RGHT \
 ),
@@ -179,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = KEYMAP( 
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_LPRN, KC_RPRN, KC_DEL,  KC_AMPR, KC_ASTR, KC_LPRN,    KC_RPRN, KC_UNDS, KC_PLUS,\
-  KC_TILD, CLEAN,SYNC,SEARCH_PROJ,SEARCH_CLASS,REFORMAT,KC_LBRC, KC_RBRC, KC_PIPE, KC_F10,  _______, _______,    KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_TILD, CLEAN,SYNC,SEARCH_PROJ,SEARCH_CLASS,REFORMAT,OPTIMPORTS, KC_RBRC, KC_PIPE, KC_F10,  _______, _______,    KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,_______, _______,  _______, _______, _______,    KC_F12, S(KC_NUHS), S(KC_NUBS),\
   _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, _______,    _______, _______, KC_MNXT\
 ),
@@ -486,6 +489,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     case KC_REFORMAT:
       if (record->event.pressed) {
          return MACRO(D(LALT),D(LGUI),T(L),U(LALT),U(LGUI),END);
+      }
+      break;
+    case KC_OPTIMPORTS:
+      if (record->event.pressed) {
+         return MACRO(D(LCTL),D(LALT),T(O),U(LCTL),U(LALT),END);
       }
       break;
     }

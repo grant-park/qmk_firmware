afd97c4c7fac83394d228c31d86c828d2f020d8c
gpark <gpark@wayfair.com>
2017-08-20 14:21:54 -0400

    updated keymap

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -7,13 +7,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
         {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
         {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-        {KC_ESC,  KC_TAB,  M(1),    KC_LGUI, KC_ENT,  KC_LSFT, KC_RCTL,    KC_SPC,  M(2),    KC_RALT, KC_QUOT, KC_BSPC}
+        {KC_ESC,  KC_TAB,  KC_LCTL, M(1),    KC_ENT,  KC_LSFT, KC_RGUI,    KC_SPC,  M(2),    KC_RALT, KC_QUOT, KC_BSPC}
     }, 
 
     [1] = {
-        {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______},
-        {_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______},
-        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+        {KC_GRV , _______, _______, _______, _______, _______, _______, _______, M(4),    KC_UP,   M(5)    ,_______},
+        {KC_TILD,  _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT,   M(6)},
+        {_______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
         {_______, _______, _______, _______, _______, _______, _______, _______, M(3),    _______, _______, _______}
     },
 
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0 },
         {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
         {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_EQL,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR},
-        {KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_GRV,  _______, KC_MINS, KC_UNDS, KC_PLUS}
+        {KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  _______, KC_MINS, KC_UNDS, KC_PLUS}
     },
 
     [3] = {
@@ -57,6 +57,16 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
                 layer_off(2);
             }
             break;
+        case 4:
+            return MACRODOWN( TYPE(KC_9), TYPE(KC_J), END);
+            break;
+        case 5:
+            return MACRODOWN( TYPE(KC_9), TYPE(KC_K), END);
+            break;
+        case 6:
+            return MACRODOWN( DOWN(KC_RGUI), TYPE(KC_SPC), UP(KC_RGUI), END);
+            break;
+
     }
     return MACRO_NONE;
 };

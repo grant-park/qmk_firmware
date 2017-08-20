998f8e4580f0168b2b1164a093e0c97a3f53e565
gpark <gpark@wayfair.com>
2017-08-20 13:26:07 -0400

    added atreus

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -42,8 +42,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ADAFRUIT_BLE_ENABLE_MODE_LEDS 0
 
 /* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { F1, D2, F0, D3, D1, D0, C6, D7, B5, B6, B7, D6 }
+#define MATRIX_ROW_PINS { F7, F6, F5, F4 }
+#define MATRIX_COL_PINS { C7, D6, B7, B6, D7, C6, D0, D1, D3, D2, F0, F1 }
 #define UNUSED_PINS
 
 #define AUDIO_VOICES
--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -1,145 +1,62 @@
 #include "planck.h"
 #include "mousekey.h"
-//#include "outputselect.h"
- 
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
-// Fillers to make layering more clear
 #define _______ KC_TRNS
-// Tap Dance Declarations
-enum {
-  TD_LAYER_MOUSE = 0
-};
-
-// Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-  // Tap once for layer, tap twice for mouse
-  [TD_LAYER_MOUSE] = ACTION_TAP_DANCE_DOUBLE(M(1),M(2))
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = {
+        {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
+        {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
+        {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
+        {KC_ESC,  KC_TAB,  M(1),    KC_LGUI, KC_ENT,  KC_LSFT, KC_RCTL,    KC_SPC,  M(2),    KC_RALT, KC_QUOT, KC_BSPC}
+    }, 
 
-// QWERTY
-[0] = {
-  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  {M(6),    KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_ENT,  KC_SPC,  M(2),    KC_RGUI, _______, M(5),    M(6)}
-}, 
+    [1] = {
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, M(3),    _______, _______, _______}
+    },
 
-// Lower
-[1] = {
-  {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS},
-  {KC_TILD, KC_F1,   KC_F2,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, KC_LCBR, KC_RCBR, KC_PIPE},
-  {MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______  , BL_DEC,  BL_INC,  _______, _______, _______, _______, _______, KC_J,    KC_9,    KC_K,    _______}
-},
+    [2] = {
+        {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0 },
+        {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
+        {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_EQL,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR},
+        {KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_GRV,  _______, KC_MINS, KC_UNDS, KC_PLUS}
+    },
 
-// Raise
-[2] = {
-  {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL },
-  {KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-},
-
-// Arrow Keys
-[3] = {
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______   , _______}
-},
-
-/* Mousekeys */
-[4] = {
-  {_______, _______, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______, _______, KC_MS_U, _______, _______, _______},
-  {_______, KC_ACL0, KC_ACL1, KC_BTN1, KC_BTN2, KC_BTN3, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_ACL1, KC_ACL2, _______, _______}
-},
-
-/* Ambi Mousekeys */
-[5] = {
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_WH_L, KC_WH_U, KC_MS_U, KC_WH_D, KC_WH_R, _______, _______, KC_WH_L, KC_WH_U, KC_MS_U, KC_WH_D, KC_WH_R},
-  {_______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______},
-  {_______, KC_BTN3, KC_BTN1, KC_BTN2, KC_ACL0, _______, _______, KC_ACL0, KC_BTN1, KC_BTN2, KC_BTN3, _______}
-}
-
+    [3] = {
+        {_______, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______, _______, _______, _______, KC_MS_U, _______, _______},
+        {KC_ACL0, KC_ACL1, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+    }
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  switch(id) {
-    case 1:
-      if (record->event.pressed) {
-        layer_on(1);
-        update_tri_layer(1,2,4);
-      } else {
-        layer_off(1);
-        update_tri_layer(1,2,4);
-      }
-      break;
-    case 2:
-      if (record->event.pressed) {
-        layer_on(2);
-        update_tri_layer(1,2,4);
-      } else {
-        layer_off(2);
-        update_tri_layer(1,2,4);
-      }
-      break;
-    case 3:
-      if (record->event.pressed) {
-          return MACRO(D(LCTL),T(LEFT),U(LCTL),END);
-      }
-      break;
-    case 4:
-      if (record->event.pressed) {
-          return MACRO(D(LCTL),T(RGHT),U(LCTL),END);
-      }
-      break;
-    case 5:
-      if (record->event.pressed) {
-          layer_on(3);
-      } else {
-          layer_off(3);
-      }
-      break;
-    case 6:
-      if (record->event.pressed) {
-          layer_on(5);
-      } else {
-          layer_off(5);
-      }
-      break;
-    // layer 7
-    case 7: 
-      if (record->event.pressed) {
-          layer_on(7);
-      } else {
-          layer_off(7);
-      }
-  }
-  return MACRO_NONE;
-
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    switch(id) {
+        case 1:
+            if (record->event.pressed) {
+                layer_on(1);
+            } else {
+                layer_off(1);
+            }
+            break;
+        case 2:
+            if (record->event.pressed) {
+                layer_on(2);
+            } else {
+                layer_off(3);
+                layer_off(2);
+            }
+            break;
+        case 3:
+            if (record->event.pressed) {
+                layer_on(3);
+            } else {
+                layer_off(3);
+                layer_off(2);
+            }
+            break;
+    }
+    return MACRO_NONE;
 };
-
-//void matrix_init_user() {
-//  _delay_ms(500); // give time for usb to initialize
-//
-//  // auto detect output on init
-//#ifdef MODULE_ADAFRUIT_BLE
-//  uint8_t output = auto_detect_output();
-//  if (output == OUTPUT_USB) {
-//    set_output(OUTPUT_USB);
-//  } else {
-//    set_output(OUTPUT_BLUETOOTH);
-//  }
-//#endif
-//}

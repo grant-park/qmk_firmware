be8764170aedbcbe7ba7e602f4d9b44d338a8449
Grant Hyun Park <grant@Grants-MacBook-Pro.local>
2017-08-23 01:59:10 -0400

    added reset to bootloader and changed mapping -- also performed dfu update, mousekeys and everything else much faster

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
-#define MANUFACTURER    Grant Hyun Park 
-#define PRODUCT         Grant's Nordic Yacht 
+#define MANUFACTURER    "Grant Hyun Park" 
+#define PRODUCT         "Grant's Nordic Yacht" 
 #define DESCRIPTION      
 
 /* key matrix size */

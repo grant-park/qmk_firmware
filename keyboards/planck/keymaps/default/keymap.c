c945f3fa0733dea0e03369cddb41076720b26a08
Grant Park <grant@MBP-3977.cable.rcn.com>
2018-09-02 18:03:34 -0400

    added more vim macros

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -28,6 +28,9 @@ enum planck_keycodes {
     VLEFT,
     VRIGHT,
     VSEARCH_REPLACE,
+    VQUIT,
+    VSAVE,
+    VQUIT_SAVE,
 
     // Firefox
     FLEFT,
@@ -74,10 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX}
     },
     [ARROWS] = {
-        {_______,            _______, _______,        _______,         _______,     XXXXXXX, XXXXXXX, _______,             _______, KC_UP,   _______,  _______},
-        {M(CLEAN),           M(SYNC), M(SEARCH_PROJ), M(SEARCH_CLASS), M(REFORMAT), XXXXXXX, XXXXXXX, M(OPTIMIZE_IMPORTS), KC_LEFT, KC_DOWN, KC_RIGHT, USEDKEY}, 
-        {M(VSEARCH_REPLACE), _______, _______,        _______,         _______,     XXXXXXX, XXXXXXX, _______,             _______, _______, _______,  _______},
-        {XXXXXXX,            XXXXXXX, XXXXXXX,        XXXXXXX,         _______,     XXXXXXX, XXXXXXX, _______,             XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX}
+        {M(VQUIT),           M(VSAVE),      _______,        _______,         _______,     XXXXXXX, XXXXXXX, _______,             _______, KC_UP,   _______,  _______},
+        {M(CLEAN),           M(SYNC),       M(SEARCH_PROJ), M(SEARCH_CLASS), M(REFORMAT), XXXXXXX, XXXXXXX, M(OPTIMIZE_IMPORTS), KC_LEFT, KC_DOWN, KC_RIGHT, USEDKEY}, 
+        {M(VSEARCH_REPLACE), M(VQUIT_SAVE), _______,        _______,         _______,     XXXXXXX, XXXXXXX, _______,             _______, _______, _______,  _______},
+        {XXXXXXX,            XXXXXXX,       XXXXXXX,        XXXXXXX,         _______,     XXXXXXX, XXXXXXX, _______,             XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX}
     },
     [DESKTOP_SWITCH] = {
         {LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),  XXXXXXX, XXXXXXX, LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0)},
@@ -125,6 +128,21 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 	    case VSEARCH_REPLACE:
 	        if (record->event.pressed) {
 	    	    return MACRO(T(S),T(SLSH),T(BSLS),D(LSFT),T(PERC),U(LSFT),D(LSFT),T(V),U(LSFT),END);
+	        }
+	        break;
+	    case VQUIT:
+	        if (record->event.pressed) {
+	    	    return MACRO(D(LSFT),T(SCLN),U(LSFT),T(Q),T(ENT),END);
+	        }
+	        break;
+	    case VSAVE:
+	        if (record->event.pressed) {
+	    	    return MACRO(D(LSFT),T(SCLN),U(LSFT),T(W),T(ENT),END);
+	        }
+	        break;
+	    case VQUIT_SAVE:
+	        if (record->event.pressed) {
+	    	    return MACRO(D(LSFT),T(SCLN),U(LSFT),T(X),T(ENT),END);
 	        }
 	        break;
     

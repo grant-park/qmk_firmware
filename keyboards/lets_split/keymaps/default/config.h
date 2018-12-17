2264feed368957c0832ca8bf57d18ed6bce9d23f
Grant Park <grant@MBP-3977.cable.rcn.com>
2018-12-16 20:22:46 -0500

    fix config

--- keyboards/lets_split/keymaps/default/config.h
+++ keyboards/lets_split/keymaps/default/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_TIME_TO_MAX    10
-#define MOUSE:uuuKEY_MAX_SPEED      5
+#define MOUSEKEY_MAX_SPEED      5
 #define MOUSEKEY_WHEEL_MAX_SPEED 1
 #define MOUSEKEY_WHEEL_DELAY 100
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 5
